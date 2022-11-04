d281c8847227276c205841038920a5a179bc2b86
PssbleTrngle <nik@somethingcatchy.net>
2022-11-04 15:41:42 +0100

    exlude items

--- common/src/main/java/com/possible_triangle/kubejs_stages/stage/StageBuilder.java
+++ common/src/main/java/com/possible_triangle/kubejs_stages/stage/StageBuilder.java
@@ -1,7 +1,10 @@
 package com.possible_triangle.kubejs_stages.stage;
 
+import java.util.List;
+
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
+
 import dev.latvian.mods.kubejs.fluid.FluidStackJS;
 import dev.latvian.mods.kubejs.item.ingredient.IngredientJS;
 import net.minecraft.resources.ResourceLocation;
@@ -14,7 +17,6 @@ public class StageBuilder {
     }
 
     private StageBuilder() {
-
     }
 
     public static StageBuilder create(Consumer consumer) {
@@ -23,11 +25,20 @@ public class StageBuilder {
         return builder;
     }
 
+    private List<IngredientJS> buildItems() {
+        var excluded = excludedItems.build();
+        return items.build().stream().map(ingredient ->
+                excluded.stream().map(IngredientJS::not).reduce(ingredient, IngredientJS::filter)
+        ).toList();
+    }
+
     public Stage build() {
-        return new Stage(defaultState, items.build(), fluids.build(), categories.build(), disguisedBlocks.build(), recipes.build());
+        return new Stage(defaultState, buildItems(), fluids.build(), categories.build(), disguisedBlocks.build(), recipes.build());
     }
 
     private final ImmutableSet.Builder<IngredientJS> items = new ImmutableSet.Builder<>();
+    private final ImmutableSet.Builder<IngredientJS> excludedItems = new ImmutableSet.Builder<>();
+
     private final ImmutableSet.Builder<FluidStackJS> fluids = new ImmutableSet.Builder<>();
     private final ImmutableSet.Builder<String> categories = new ImmutableSet.Builder<>();
     private final ImmutableMap.Builder<Block, Block> disguisedBlocks = new ImmutableMap.Builder<>();
@@ -42,15 +53,21 @@ public class StageBuilder {
     public void disabledByDefault() {
         setDefaultState(false);
     }
+
     public void setDefaultState(boolean value) {
         setDefaultState(ThreeState.of(value));
     }
+
     public void setDefaultState(ThreeState state) {
         defaultState = state;
     }
 
     public void addItem(IngredientJS ingredient) {
         items.add(ingredient);
+    }
+
+    public void excludeItem(IngredientJS ingredient) {
+        excludedItems.add(ingredient);
     }
 
     public void addFluid(FluidStackJS fluid) {
