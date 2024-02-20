11338a4d7623ac4f0459539c6bcc4112183f4be0
Niklas Widmann <nik@somethingcatchy.net>
2024-02-20 19:51:24 +0100

    replace uses of String IDs with ResourceLocation

--- common/src/main/java/com/possible_triangle/stages/StageReloadListener.java
+++ common/src/main/java/com/possible_triangle/stages/StageReloadListener.java
@@ -16,7 +16,6 @@ import net.minecraft.util.profiling.ProfilerFiller;
 import net.minecraft.world.item.crafting.Ingredient;
 
 import java.util.Map;
-import java.util.Objects;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
